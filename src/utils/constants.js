a654d154541b017d44ddb176fd125f2567113a5e
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-03-01 13:49:35 +0530

    Add RestaurantMenu component and integrate it into routing

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/static-assets/images/swiggy_logo_white.png";
+
+export const MENU_API = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.97530&lng=77.59100&restaurantId=";
